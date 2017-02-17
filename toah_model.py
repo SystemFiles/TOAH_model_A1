898b88380c62e7f65dad4d15f543eb0e64c547df
Benjamin Sykes <bensykes12@gmail.com>
2017-02-17 15:34:22 -0500

    Most up to date.

--- toah_model.py
+++ toah_model.py
@@ -54,21 +54,25 @@ class TOAHModel:
         self._stools = []
 
         # create stools from ^ Number of stools
-        for i in range(number_of_stools):
+        i = 0
+        while i < self._number_stools:
             self._stools.append([])
+            i += 1
+            
         self._move_seq = MoveSequence([])
 
         # self.fill_first_stool(4) # Temp
 
-    def fill_first_stool(self: 'TOAHModel', number_of_cheeses:int) -> None:
+    def fill_first_stool(self: 'TOAHModel', number_of_cheeses: int) -> None:
         '''Fills the first stool with a certain number of cheeses
         defined by the caller.
         '''
 
         if self.get_number_of_cheeses() > 0:
-            raise IllegalMoveError("You can't fill a stool with cheeses already present..")
+            raise IllegalMoveError("You can't fill a stool",
+                                   "with cheeses already present...")
         else:
-            for i in range(number_of_cheeses, 0, -1):  # Bigger on bottom of stack.
+            for i in range(number_of_cheeses, 0, -1):#Bigger on bottom of stack
                 self._stools[0].append(Cheese(i))
 
         self._number_of_cheeses = number_of_cheeses
@@ -81,37 +85,38 @@ class TOAHModel:
     def number_of_moves(self: 'TOAHModel') -> int:
         '''Returns the total number of moves made by this TOAHmodel.'''
 
-        return self.get_move_seq().length()
+        return self._move_seq.length()
 
-    def get_height_of_stool(self:'TOAHModel', stool_index:int) -> int:
+    def get_height_of_stool(self: 'TOAHModel', stool_index: int) -> int:
         '''Returns the height of the current stool at stool_index.'''
 
         return len(self._stools[stool_index])
 
-    def add(self: 'TOAHModel', cheese: 'Cheese', stool_index:int) -> None:
+    def add(self: 'TOAHModel', cheese: 'Cheese', stool_index: int) -> None:
         '''Adds a cheese to the toah_model in amount n..'''
             ## TODO: Fix Add method. There is an issue here says Dan.
         if self.get_top_cheese(stool_index) is not None:
             if self.get_top_cheese(stool_index).size <= cheese.size:
-                raise IllegalMoveError("Illegal move. You cannot add a smaller cheese to a larger one..")
+                raise IllegalMoveError("Cant add larger cheese on smaller one")
             else:
                 try:
                     self._stools[stool_index].append(cheese)
                     return
                 except IndexError:
-                    raise IllegalMoveError("Stool DNE...")
+                    raise IllegalMoveError("not working ffs")
         else:
             try:
                 self._stools[stool_index].append(cheese)
                 return
             except IndexError:
-                raise IllegalMoveError("Invalid Move. Cannot Add cheese to place with no stool.")
+                raise IllegalMoveError("Cant Add cheese to place with no stool")
         return
 
     def get_cheese_location(self: 'TOAHModel', cheese: 'Cheese') -> int:
         '''Returns the location of a cheese.'''
 
-        # Go through each stool, and see if the Cheese object is in that stool. If so return stool index.
+        # Go through each stool, and see if the Cheese
+        # object is in that stool. If so return stool index.
         # If not, return 0 (TEMP)
         for i in range(self.get_number_of_stools()):
             for j in range(len(self._stools[i])):
@@ -124,7 +129,7 @@ class TOAHModel:
 
         return self._number_of_cheeses
 
-    def get_top_cheese(self: 'TOAHModel', stool_index:int) -> 'Cheese':
+    def get_top_cheese(self: 'TOAHModel', stool_index: int) -> 'Cheese':
         '''Returns the top cheese object on the platform.'''
 
         # print(self._stools) # TEMP debugging
@@ -138,7 +143,7 @@ class TOAHModel:
             else:
                 return None
         except IndexError:
-            raise IllegalMoveError("Tried get cheese at stool that doesn't exist.")
+            raise IllegalMoveError("Stool does not exist.")
 
     def remove_top_cheese(self: 'TOAHModel', stool_index) -> 'Cheese':
         '''Removes the top cheese on stool at stool_index.'''
@@ -146,18 +151,20 @@ class TOAHModel:
         try:
             return self._stools[stool_index].pop()  # Maybe a problem here.
         except IndexError:
-            raise IllegalMoveError("Cannot move cheese from stool with no cheese")
+            raise IllegalMoveError("Stool has no cheese")
 
-    def move(self: 'TOAHModel', start_stool:int, dest_stool:int) -> None:
+    def move(self: 'TOAHModel', start_stool: int, dest_stool: int) -> None:
         '''Moves the cheese object to location.'''
 
-        if (start_stool > len(self._stools)) or (dest_stool > len(self._stools)) or (start_stool == dest_stool):
+        if (start_stool > len(self._stools)) or \
+           (dest_stool > len(self._stools)) or \
+           (start_stool == dest_stool):
             raise IllegalMoveError("Stool does not exist or is the same stool.")
         else:
             cheese_move = self.remove_top_cheese(start_stool)
             try:
                 self.add(cheese_move, dest_stool)
-                self.get_move_seq().add_move(start_stool, dest_stool)
+                self._move_seq.add_move(start_stool, dest_stool)
             except IllegalMoveError:
                 raise IllegalMoveError
         return
@@ -177,6 +184,7 @@ class TOAHModel:
         print(self)
         if self._number_of_cheeses == len(self._stools[-1]):
             print("Well done! You've completed the game")
+            #print("Move sequence: ", self._move_seq._moves)
             return True
         else:
             return False
@@ -375,6 +383,22 @@ class MoveSequence(object):
         for move in self._moves:
             model.move(move[0], move[1])
         return model
+
+    def __eq__(self: 'MoveSequence', other: 'MoveSequence') -> bool:
+        '''
+        Return True iff contents of self is the same as the contents of other
+        Other has to be MoveSequence object
+        '''
+        if len(self._moves) != len(other._moves):
+            return False
+        for i in range(len(self._moves)):
+            if self._moves[i] != other._moves[i]:
+                return False
+        return True
+
+        
+    def __repr__(self):
+        return str(self._moves)
 
 if __name__ == '__main__':
     import doctest
